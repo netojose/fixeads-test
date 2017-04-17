59f86efde401fd5540db883dd3f92eac61f99b03
José Neto <sputinykster@gmail.com>
2017-04-16 21:46:44 -0300

    Fix server bug

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const path = require('path');
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/build/'));
 
 app.listen(process.env.PORT || 8080);
+
+app.get('*', function (request, response){
+    response.sendFile( __dirname + path.resolve(__dirname, '/build/index.html') );
+});
